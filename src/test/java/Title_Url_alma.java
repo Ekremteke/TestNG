c2362e38c94e269446967fd6d8f598c604a22c99
Ekremteke <ekremteke.uk@gmail.com>
2022-05-06 21:02:23 +0100

    fifth

---
+++ src/test/java/Title_Url_alma.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Title_Url_alma {
+
+    static WebDriverManager driver;
+
+    @Test
+    public void driver_Tanimlama() {
+
+        WebDriver driver;
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+
+        String titleGoogle=driver.getTitle();
+        System.out.println("GoogleTitle => "+ titleGoogle);
+
+        driver.navigate().to("https://amazon.com");
+
+        String titleAmazon=driver.getTitle();
+        System.out.println("AmazonTitle ==> "+titleAmazon);
+
+        String urlAmazon= driver.getCurrentUrl();
+        System.out.println("UrlAmazon => "+urlAmazon);
+
+        if(urlAmazon.contains("amazon")){
+            System.out.println("Var 'amazon' " + urlAmazon);
+        }
+        else{
+            System.out.println("Yok " + urlAmazon);
+        }
+
+
+    }
+}

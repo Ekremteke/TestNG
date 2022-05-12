9f188709fcdb1a84a2eda580fd9d1dc674db621b
Ekremteke <ekremteke.uk@gmail.com>
2022-05-12 18:16:56 +0100

    grup calismasi

--- src/test/java/Ders2/HW.java
+++ src/test/java/Ders2/HW.java
@@ -2,15 +2,15 @@ package Ders2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class tagName {
+public class HW {
     static WebDriver driver;
 
     @Test
@@ -27,16 +27,14 @@ public class tagName {
         Thread.sleep(3000);
         guvenlikDuvari.click();
 
-        WebElement yazi = driver.findElement(By.tagName("a"));
-        System.out.println("en bastaki a tag name =>" + yazi.getText());
+        WebElement catalog=driver.findElement(By.name("q"));
+        catalog.sendKeys("city bike"+ Keys.ENTER);
+//        catalog.submit();
 
-        System.out.println("--------------");
+        WebElement kac = driver.findElement(By.id("result-stats"));
+        System.out.println(kac.getText());
 
-        List<WebElement> listem=driver.findElements(By.tagName("a"));
-
-        for (WebElement w:listem){
-//            System.out.println(""+ listem.getText());
-     }
-
+        WebElement shopping = driver.findElement(By.partialLinkText("Shopping"));
+        shopping.click();
 
     }}

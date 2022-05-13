0e19d0144333fe4180b857382ae19e33ee79cc2d
Ekremteke <ekremteke.uk@gmail.com>
2022-05-13 21:16:51 +0100

    After Before method mix

--- src/test/java/Ders_4/BeforeMethod_AfterMethod.java
+++ src/test/java/Ders_4/BeforeMethod_AfterMethod.java
@@ -3,33 +3,6 @@ package Ders_4;
 import org.testng.annotations.*;
 
 public class BeforeMethod_AfterMethod {
-    @BeforeClass
-    public static void setUp(){
-        System.out.println("Before class calisti");
-        //Gidecegimiz sayfaya gidiyoruz
-    }
-
-    @BeforeMethod
-    public void testtenOnce(){
-        System.out.println("Testten once calisti");
-    }
-
-    @Test
-    public void test1(){
-        System.out.println("test 1  calisti");
-        //  testlerimizi calistiriyoruz
-
-    }
-    @Test
-    public void test2(){
-        System.out.println("test 2 calisti");
-        //  testlerimizi calistiriyoruz
-    }
-
-    @AfterMethod
-    public void testtenSonra(){
-        System.out.println("testten sonra caisti");
-    }
 
     @AfterClass
     public static void teardown(){
@@ -37,7 +10,30 @@ public class BeforeMethod_AfterMethod {
         System.out.println("After class calisti");
     }
 
+    @Test
+    public void test1(){
+        System.out.println("test 1  calisti");
+        //  testlerimizi calistiriyoruz
+    }
 
+    @AfterMethod
+    public void testtenSonra(){
+        System.out.println("Testten sonra calisti");
+    }
+    @BeforeClass
+    public static void setUp(){
+        System.out.println("Before class calisti");
+        //Gidecegimiz sayfaya gidiyoruz
+    }
+    @BeforeMethod
+    public void testtenOnce(){
+        System.out.println("Testten once calisti");
+    }
+    @Test
+    public void test2(){
+        System.out.println("test 2 calisti");
+        //  testlerimizi calistiriyoruz
+    }
 
 
 

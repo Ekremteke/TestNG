16303ac56f41f7ecef10569093f561df14ce7c0c
Ekremteke <ekremteke.uk@gmail.com>
2022-05-13 19:30:46 +0100

    whole xpath

--- src/test/java/ders_3/xpath.java
+++ src/test/java/ders_3/xpath.java
@@ -1,6 +1,9 @@
-package ders_3;
+package Ders_3;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -8,22 +11,27 @@ import java.util.concurrent.TimeUnit;
 
 public class xpath {
 
-    ChromeDriver driver;
+    WebDriver driver;
     @Test
     public void setup(){
-
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-
+        driver =new ChromeDriver();
         driver.manage().window().maximize();
-
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.get("http://gsreddy.com/project/admin/login.php");
+        driver.get("http://gcreddy.com/project/admin/login.php");
 
+        WebElement userName= driver.findElement(By.xpath("//input[@type='text']"));
+        userName.sendKeys("gcreddy");
 
+        WebElement pass=driver.findElement(By.xpath("//input[@type='password']"));
+        pass.sendKeys("Temp@2020");
 
+        WebElement login=driver.findElement(By.xpath("//button[@id='tdb1']"));
+        login.click();
 
 
     }
+
+
 }

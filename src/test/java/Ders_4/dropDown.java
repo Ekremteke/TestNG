87aa6badef3a030f29abe0ac33ef04a68f07db56
Ekremteke <ekremteke.uk@gmail.com>
2022-05-17 20:14:15 +0100

    second commit about dropDown

--- src/test/java/Ders_4/dropDown.java
+++ src/test/java/Ders_4/dropDown.java
@@ -1,4 +1,52 @@
 package Ders_4;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class dropDown {
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @BeforeMethod
+    public void beforeMethod() throws InterruptedException {
+        driver.get("https://amazon.com");
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        WebElement dropDown = driver.findElement(By.id("searchDropdownBox"));
+
+        Select select = new Select(dropDown);
+
+        select.selectByVisibleText("Baby");
+
+        Thread.sleep(3000);
+
+        select.selectByIndex(5); // 5inci siradaki degere gidiyor
+
+        Thread.sleep(3000);
+
+       select.selectByValue("search-alias=computers-intl-ship"); // computersa gidiyor
+
+        WebElement election = select.getFirstSelectedOption();
+        System.out.println("son secili element==> " + election.getText());
+
+
+    }
 }
